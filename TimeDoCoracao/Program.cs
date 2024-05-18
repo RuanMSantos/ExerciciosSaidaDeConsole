c86b6622c254e2fded5c3760b73f0ad1765144d9
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-17 22:14:56 -0300

    Exercício finalizado TimeDoCoracao

---
+++ TimeDoCoracao/Program.cs
@@ -0,0 +1,58 @@
+﻿Console.Clear();
+ 
+Console.WriteLine("--- Time do coração ---\n");
+ 
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("C");
+Console.ResetColor();
+ 
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("O");
+Console.ResetColor();
+ 
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("R");
+Console.ResetColor();
+ 
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("I");
+Console.ResetColor();
+ 
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("N");
+Console.ResetColor();
+ 
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("T");
+Console.ResetColor();
+ 
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("H");
+Console.ResetColor();
+ 
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("I");
+Console.ResetColor();
+ 
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("A");
+Console.ResetColor();
+ 
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("N");
+Console.ResetColor();
+ 
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("S");
+Console.ResetColor();
