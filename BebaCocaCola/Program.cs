16d8ee009ee2bf0f9a687c8598ebca2404870b92
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-17 22:11:35 -0300

    Exercício finalizado BebaCocaCola

---
+++ BebaCocaCola/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+ 
+Console.WriteLine("--- Beba Coca Cola ---\n");
+ 
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Red;
+Console.WriteLine(@"
+     beba  coca  cola
+     babe        cola
+     beba  coca      
+     babe  cola  caco
+     caco            
+     cola            
+          c l o a c a ");
+ 
+Console.ResetColor();
