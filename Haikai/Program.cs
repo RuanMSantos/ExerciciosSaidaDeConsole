176a9108ea113374742552be2cadb52204e09e95
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-17 21:56:48 -0300

    Exercício finalizado Haikai

---
+++ Haikai/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+ 
+Console.WriteLine("--- Haikai ---");
+ 
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("\nInverno");
+Console.ResetColor();
+ 
+Console.WriteLine("\nPingentes pendurados");
+Console.WriteLine("Neve cobrindo o chão frio");
+Console.WriteLine("Aconchegante perto do fogo\n");
