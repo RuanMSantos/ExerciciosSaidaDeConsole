672bef2fe5ed27611d0871484c186df94327496f
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-17 22:01:24 -0300

    Exercício finalizado Piada

---
+++ Piada/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+ 
+Console.WriteLine("--- Piada ---");
+ 
+Console.WriteLine("\nVocê já ouviu falar do cara que roubou o calendário?");
+ 
+Console.WriteLine("\nEle pegou 12 meses!");
