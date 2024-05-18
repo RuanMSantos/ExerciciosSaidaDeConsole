b5446859ea79f252ea176bf6658d50e6d3df28e8
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-17 21:51:18 -0300

    Exercício finalizado FrutasPreferidas

---
+++ FrutasPreferidas/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+ 
+Console.WriteLine("--- Minhas frutas preferidas ---");
+ 
+Console.WriteLine("\n1. BANANA");
+Console.WriteLine("2. MAMÃO");
+Console.WriteLine("3. LARANJA");
+Console.WriteLine("4. MELÂNCIA");
+Console.WriteLine("5. MORANGO");
