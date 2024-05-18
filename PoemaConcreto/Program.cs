7a1cdef985b1aed1893a2999ca95f95a1e6dd615
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-17 22:08:18 -0300

    Exercício finalizado PoemaConcreto

---
+++ PoemaConcreto/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+ 
+Console.WriteLine("--- Poema concreto ---\n");
+ 
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine(@"   u m 
+     m o v i
+     mento
+   compondo
+  além 
+         da
+ nuvem 
+    um 
+   campo
+      de
+   combate
+ 
+     mira
+   gem 
+      ira
+         de
+    um 
+      horizonte
+puro
+   num 
+     mo
+     mento
+ vivo");
+ 
+Console.ResetColor();
