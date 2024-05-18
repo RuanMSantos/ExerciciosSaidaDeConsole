30602a4ee02e791e770bb95d60b86486616adc3c
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-17 21:42:49 -0300

    Exercício finalizado Continue

---
+++ Continue/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+ 
+Console.WriteLine("Pressione para continuar...");
+ 
+Console.Read();
+ 
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Parabéns você concluiu o programa!");
+Console.ResetColor();
