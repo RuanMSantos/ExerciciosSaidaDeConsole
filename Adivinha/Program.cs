d532ace5f249ae729dd269d202ee2e4bdb0dacb9
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-17 22:04:50 -0300

    Exercício finalizado Adivinha

---
+++ Adivinha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+ 
+Console.WriteLine("--- Adivinha ---");
+ 
+Console.WriteLine("\nO que é, o que é...");
+ 
+Console.WriteLine("...Assador de sino?");
+ 
+Thread.Sleep(5000);
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("\nAssa sino");
+Console.ResetColor();
