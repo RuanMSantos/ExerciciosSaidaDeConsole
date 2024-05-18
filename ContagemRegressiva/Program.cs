cd40d94bea094fe37dff80c07be1f5c123c56ec3
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-17 21:20:04 -0300

    Exercício finalizado ContagemRegressiva

---
+++ ContagemRegressiva/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+ 
+Console.WriteLine("---- Contagem regressiva ----");
+ 
+Thread.Sleep(1000);
+Console.WriteLine("\nPreparar...");
+ 
+Thread.Sleep(1000);
+Console.WriteLine("\nApontar...");
+ 
+Thread.Sleep(1000);
+Console.WriteLine("\nFOGO!");
